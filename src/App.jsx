3b117a073778f7fe6169f4c08b0e8dbf845ed6b2
Nilesh Jain <neeljain7821@gmail.com>
2023-06-20 08:42:41 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Hero from './components/Hero'
+import Navigation from './components/Navigation'
+import "./App.css"
+
+const App=()=> {
+
+  return (
+    <>
+      <Navigation />
+      <Hero />
+     
+    </>
+  )
+}
+
+export default App
